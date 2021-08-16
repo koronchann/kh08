880f7e2989bcf2081367da253307a283ebf02a10
mjkim <mjkim@DESKTOP-JFMHTMJ>
2021-08-16 11:15:09 +0900

    Calculate 연습

---
+++ day04/src/Calculate.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+public class Calculate {
+	public static void main(String[] args) {
+		System.out.println("2021-08-16");
+		System.out.println(2021-8-16);
+		System.out.println(5000*3);
+	}
+}
